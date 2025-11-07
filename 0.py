4160ab940f1d4b8d046c2e6f19f125a83b103d5e
K9BFF <masoncodes0@gmail.com>
2025-11-06 21:26:33 -0600

    huh

--- 0.py
+++ 0.py
@@ -5,3 +5,11 @@
 # for example, 25 is a square number because 5**2 = 5*5 = 25; it is also an odd square.
 # the first 5 square numbers are 1, 4, 9, 16, and 25, and the sum of the odd squares is 1+9+25 = 35
 # among the first 742 thousand square numbers, what is the sum of all the odd squares?
+
+import math
+
+print("Problem 0")
+
+#could modulo to find odd
+
+for i in loop:
