7555fa4974d20f1f58eb019d1662136b7159839f
K9BFF <masoncodes0@gmail.com>
2025-11-06 21:07:12 -0600

    added problem 0

---
+++ 0.py
@@ -0,0 +1,7 @@
+# project euler
+# problem 0
+
+# a number is a perfect square, or a square number, if it is the square of a positive integer.
+# for example, 25 is a square number because 5**2 = 5*5 = 25; it is also an odd square.
+# the first 5 square numbers are 1, 4, 9, 16, and 25, and the sum of the odd squares is 1+9+25 = 35
+# among the first 742 thousand square numbers, what is the sum of all the odd squares?
